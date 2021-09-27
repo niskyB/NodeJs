a0dcbdb315213da6bd2ad651db91e6af56da2c7f
Loc <hoanglocst900@gmail.com>
2021-09-27 19:33:10 +0700

    using confid

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,9 @@
+const config = require('config');
 const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express();
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
@@ -9,6 +12,17 @@ console.log(`app: ${app.get('env')}`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
 
 app.use(logger);
 
