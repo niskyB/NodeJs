1f3ba9fa8b0444bada5cb73e572e606f0b0a54ae
Loc <hoanglocst900@gmail.com>
2021-09-28 21:09:26 +0700

    create course in db

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,27 @@ const mongoosse = require('mongoose');
 mongoosse.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to mongodb'))
     .catch(err => console.error('Could not connect to mongodb...', err));
+
+const courseSchema = new mongoosse.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoosse.model('Course', courseSchema);
+
+async function creatCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+creatCourse();
