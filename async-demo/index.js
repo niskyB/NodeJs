e680b9030dfafa3336da4ab9728860b12b4d944c
Loc <hoanglocst900@gmail.com>
2021-09-28 11:10:26 +0700

    consuming promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,4 +1,11 @@
-getUser(1, getRepository);
+// getUser(1, getRepository);
+
+
+getUser(1)
+    .then(user => getRepository(user.gitHubUsername))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => displayCommits(commits))
+    .catch(err => console.log(err.message));
 
 function displayCommits(commits) {
     console.log('Commits', commits);
@@ -12,23 +19,29 @@ function getRepository(user) {
     getRepository(user.gitHubUsername, getCommits);
 }
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database');
-        callback({ id: id, gitHubUsername: 'niskyB' });
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database');
+            resolve({ id: id, gitHubUsername: 'niskyB' });
+        }, 2000);
+    });
 }
 
-function getRepository(username, callback) {
-    setTimeout(() => {
-        console.log('Reading a repository from github');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepository(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a repository from github');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    })
 }
 
-function getCommits(repo, callback) {
-    setTimeout(() => {
-        console.log('Reading commits from a repository');
-        callback(['commit1', 'commit2', 'commit3']);
-    }, 2000);
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading commits from a repository');
+            resolve(['commit1', 'commit2', 'commit3']);
+        }, 2000);
+    })
 }
