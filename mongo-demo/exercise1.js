6f0117ecd4ae285d7a5a92d811ff3a3514d39e4f
Loc <hoanglocst900@gmail.com>
2021-09-29 14:18:04 +0700

    simple query

---
+++ mongo-demo/exercise1.js
@@ -0,0 +1,28 @@
+const mongoosse = require('mongoose');
+mongoosse.connect('mongodb://localhost/mongo-exercises')
+    .then(() => console.log('Connect to mongod server'))
+    .catch(err => console.error(err));
+
+const schema = new mongoosse.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: { type: Boolean, default: true },
+    price: { type: Number, default: 0 }
+});
+
+const Course = mongoosse.model('Course', schema);
+
+async function getCourses() {
+    return courses = await Course.find({ isPublished: true, tags: 'backend' })
+        .sort({ name: 1 })
+        .select({ name: 1, author: 1 });
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
