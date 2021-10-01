76e6067c372572938dd0ca7c35718806923f1c1c
Loc <hoanglocst900@gmail.com>
2021-10-01 20:12:30 +0700

    using array of sub-document

--- relationshipdb/embedding.js
+++ relationshipdb/embedding.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playgroundV3')
+mongoose.connect('mongodb://localhost/playgroundV4')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -14,13 +14,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
     name: String,
-    author: authorSchema
+    authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
     const course = new Course({
         name,
-        author
+        authors
     });
 
     const result = await course.save();
@@ -43,6 +43,26 @@ async function updateAuthor(courseId) {
     });
 }
 
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAuthor(courseId, author) {
+    const course = await Course.findById(courseId);
+    course.authors.push(author);
+    course.save();
+}
 
-updateAuthor('615702d19c73093e15c80773');
+async function removeAuthor(courseId, authorId) {
+    const course = await Course.findById(courseId);
+    const author = course.authors.id(authorId);
+    author.remove();
+    course.save();
+}
+
+// createCourse('Node Course', [
+//     new Author({ name: 'Mosh' }),
+//     new Author({ name: 'John' })
+// ]);
+
+// updateAuthor('615702d19c73093e15c80773');
+
+// addAuthor('615707637b5660be7de01f4e', new Author({ name: 'Loc' }))
+
+removeAuthor('615707637b5660be7de01f4e', '6157089d5592d78b3acd60a3');
