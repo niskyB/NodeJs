d19d4631e19eda266cc5726d8c3dfde8be1e10ec
Loc <hoanglocst900@gmail.com>
2021-10-03 02:40:12 +0700

    add user model

---
+++ express-demo/models/user.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        minLength: 5,
+        maxLength: 50,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        minLength: 0,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        minLength: 6,
+        maxLength: 50,
+        required: true
+    }
+}));
+
+function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(0).email().required(),
+        password: Joi.string().min(6).max(50).required()
+    });
+
+    const { error } = schema.validate(user);
+    return error;
+}
+
+exports.User = User;
+exports.validate = validateUser;
