da81952ec30a68c148743bfd713f70d4b0727973
Loc <hoanglocst900@gmail.com>
2021-10-03 03:10:35 +0700

    add user route

--- express-demo/vidly.js
+++ express-demo/vidly.js
@@ -3,6 +3,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
@@ -15,6 +16,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 
