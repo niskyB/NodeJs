6e5a5919ba39f3e4788edbf9163f971e144adbb6
Loc <hoanglocst900@gmail.com>
2021-10-03 22:03:10 +0700

    add /me

--- express-demo/routes/users.js
+++ express-demo/routes/users.js
@@ -3,6 +3,12 @@ const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+
+router.get('/me', auth, async(req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+})
 
 router.post('/', async(req, res) => {
     const error = validate(req.body);
