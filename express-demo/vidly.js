6b089bab21b6339429060f451ea3cce916af7694
Loc <hoanglocst900@gmail.com>
2021-10-05 17:09:53 +0700

    extracting routes

--- express-demo/vidly.js
+++ express-demo/vidly.js
@@ -2,16 +2,11 @@ require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
 const express = require('express');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('config');
+
+require('./startup/routes')(app);
 
 process.on('uncaughtException', (ex) => {
     winston.error(ex.message, ex);
@@ -37,13 +32,7 @@ mongoose.connect('mongodb://localhost/playground')
 
 app.use(express.json());
 
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+
 
 const port = process.env.PORT || 3000;
 
